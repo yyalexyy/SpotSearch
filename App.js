2a9dcdb5dbd86d497aa8a4188c7f5972babbb81c
yyalexyy <44172655+ajuan3@users.noreply.github.com>
2020-06-15 20:27:41 -0400

    Added styles and icon, logo, and splash png

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.firstPage}>
+        What can I do for you today?
+      </Text>
     </View>
   );
 }
@@ -12,8 +14,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+  },
+  firstPage: {
+    color: '#fff',
+    fontSize: 30,
+    marginHorizontal: 15,
+    marginBottom: 15,
+    marginTop: 15,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 20,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
   },
 });
