4dcc8f0ac0466c18f54de1d8443e5f5dc2d0042c
rrosyidi <ravioli4stuff@gmail.com>
2020-06-16 18:00:26 -0400

    Front Page

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image, StyleSheet, Text, TouchableOpacity, View, Animated } from 'react-native';
+import {Button, Image, StyleSheet, Text, TouchableOpacity, View, Animated, Alert } from 'react-native';
 import logo from './assets/logo.png';
 import { SplashScreen } from 'expo'
 SplashScreen.preventAutoHide();
@@ -8,19 +8,38 @@ setTimeout(SplashScreen.hide, 4000);
 export default function App() {
   return (
     <View style={styles.container}>
+
       <Text style={styles.firstPage}>
         What can I do for you today?
       </Text>
+
+      <View style={styles.box1}>
+        <Button 
+        color='white'
+        title="Looking for a place to eat"
+        onPress={() => Alert.alert('Hello I have been pushed')}/>
+      </View>
+
+      <View style={styles.box2}>
+        <Button 
+        color='white'
+        title="Looking for a place to "
+        onPress={() => Alert.alert('Hello I have been pushed')}/>
+      </View>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    position: 'absolute',
+    top: 50,
+    left: 20, 
+    backgroundColor: '#FFFFFF',
     flex: 1,
-    backgroundColor: '#000000',
+    flexDirection: 'column', 
     alignItems: 'center',
-    justifyContent: 'center',
   },
   logo: {
     width: 305,
@@ -28,19 +47,29 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   firstPage: {
-    color: '#fff',
-    fontSize: 30,
+    color: '#9F9F9F',
+    fontSize: 25,
     marginHorizontal: 15,
     marginBottom: 15,
     marginTop: 15,
   },
-  button: {
-    backgroundColor: "blue",
-    padding: 20,
-    borderRadius: 20,
+  box1: {
+    position: 'absolute',
+    top: 100,
+    backgroundColor: '#9F9F9F',
+    margin: 10,
+    padding: 30,
+    textAlign: "center",
+    borderRadius: 45, 
   },
-  buttonText: {
-    fontSize: 20,
-    color: '#fff',
+  box2: {
+    position: "absolute",
+    top: 250,
+    padding: 30,
+    width: 280,
+    backgroundColor: '#9F9F9F',
+    margin: 10,
+    
+    borderRadius: 45, 
   },
 });
