a22819e8434fcbcd654d390a132b7c72ee6126c7
rrosyidi <ravioli4stuff@gmail.com>
2020-07-10 15:39:35 -0400

    Adjusted Color Palette

--- screens/HomePage.js
+++ screens/HomePage.js
@@ -18,15 +18,16 @@ function Separate() {
   return <View style = {{
     position: 'relative',
     marginTop: 20,
-    borderBottomColor: 'white',
-    borderBottomWidth: 5,
+    height: 5,
+    backgroundColor: '#B8E5FF',
     borderRadius: 10,
     marginLeft: 15,
     marginRight: 15,
-    shadowOffset: { width: 0, height: 4},
-    shadowOpacity: .8,
-    shadowRadius: 3,
-    zIndex: 999
+    shadowColor: '#000',
+    shadowOffset: { width: 1, height: 3},
+    shadowOpacity: .6,
+    shadowRadius: 2,
+    zIndex: 999,
     }} />;
 }
 /**
@@ -40,8 +41,9 @@ Ex. Display a button, and then:
 */
 export const HomePage = ({ navigation }) => {
     return (
-      <SafeAreaView backgroundColor = '#3AA4E0'>
-
+      
+      <SafeAreaView backgroundColor = '#B8E5FF'> 
+          {/**#3AA4E0 */}
           <View style = {styles.topBox}>
 
             <View style = {{flexDirection: 'row'}}>
@@ -54,8 +56,8 @@ export const HomePage = ({ navigation }) => {
           </View>
        
 
-          <View style = {{backgroundColor: '#0E2163', marginTop: 10, marginBottom: 170, borderTopLeftRadius: 25, borderTopRightRadius: 25}}>
-
+          <View style = {{backgroundColor: '#FFFFFF', marginTop: 10, marginBottom: 170, borderTopLeftRadius: 25, borderTopRightRadius: 25,}}>
+            {/**#0E2163 */}
             <Separate/>
 
             {/* oriented by column */}
@@ -226,6 +228,10 @@ const styles = StyleSheet.create({
       borderRadius: 10,
       width: wp('42.1%'),     //158
       height: hp('23.7%'),    //148
+      shadowColor: '#000',
+      shadowOffset: { width: 1, height: 4 },
+      shadowOpacity: .8,
+      shadowRadius: 3,
     },
   
     rightBox: {
@@ -235,6 +241,10 @@ const styles = StyleSheet.create({
       borderRadius: 10,
       width: wp('42.1%'),
       height: hp('23.7%'),
+      shadowColor: '#000',
+      shadowOffset: { width: 1, height: 4 },
+      shadowOpacity: .8,
+      shadowRadius: 3,
     },
   
     buttonResize: {
@@ -249,8 +259,6 @@ const styles = StyleSheet.create({
     buttons: {
       alignItems: 'center',
       borderRadius: 10,
-      borderColor: 'black',
-      borderWidth: 2,
       height: wp('42%'), //154
     },
   
@@ -263,12 +271,10 @@ const styles = StyleSheet.create({
       height: wp('42%'), //154
       justifyContent: 'center',
       alignItems: 'center',
-      shadowColor: '#000',
-      shadowOffset: { width: 2, height: 3 },
+      shadowColor: 'black',
+      shadowOffset: { height: 5 },
       shadowOpacity: 0.8,
       shadowRadius: 5,
-      borderColor: 'black',
-      borderWidth: 2
     },
 
     boxText: {
@@ -282,15 +288,12 @@ const styles = StyleSheet.create({
 
     bottomTextBorder: {
       position: 'absolute' ,
-      top: hp('17.4%'),  //108
-      left: -2,
+      top: hp('17.7%'),  //108
       height: hp('6%'),       //40
       width: wp('42.1%'),     //158
       backgroundColor: 'white', 
       borderBottomLeftRadius: 10, 
       borderBottomRightRadius: 10,
-      borderColor: 'black',
-      borderWidth: 2
     }
 
   });
