bf905ad7ec71a65a7ad09d991bee17a69f2d2b4b
rrosyidi <ravioli4stuff@gmail.com>
2020-07-20 18:34:35 -0400

    'redesign'

---
+++ screens/HomePageRedesign.js
@@ -0,0 +1,15 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';  // import safe areas to display on screen
+import { ScrollView, Button, Image, StyleSheet, Text, TouchableOpacity, View, Animated} from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+//import logo from './assets/logo.png';     //import logo
+import { NavigationContainer, DrawerActions } from '@react-navigation/native';
+import { createStackNavigator, Assets } from '@react-navigation/stack';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
+import { render } from 'react-dom';
