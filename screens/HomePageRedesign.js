6343146373b07e71c3ad5cdd8f990fa4d97d7e83
rrosyidi <ravioli4stuff@gmail.com>
2020-07-22 20:22:19 -0400

    Fixing things

--- screens/HomePageRedesign.js
+++ screens/HomePageRedesign.js
@@ -1,253 +1,274 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';  // import safe areas to display on screen
-import { ScrollView, Button, Image, StyleSheet, Text, TouchableOpacity, View, Animated} from 'react-native';
+import { ScrollView, Button, Image, StyleSheet, Text, TouchableOpacity, View, Animated } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 //import logo from './assets/logo.png';     //import logo
 import { NavigationContainer, DrawerActions } from '@react-navigation/native';
 import { createStackNavigator, Assets } from '@react-navigation/stack';
 import {
-  createDrawerNavigator,
-  DrawerContentScrollView,
-  DrawerItemList,
-  DrawerItem,
+    createDrawerNavigator,
+    DrawerContentScrollView,
+    DrawerItemList,
+    DrawerItem,
 } from '@react-navigation/drawer';
 import { render } from 'react-dom';
-
+ 
 export class HomePageRedesign extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            toggle: false
+            toggle: false,
+            dinDateOp: .5,
+            foodAreaOp: .5,
+            vacSpotOp: .5,
+            hangOutOp: .5,
+            continColor: 'white'
         };
     }
 
-    Toggle = () => {
-        this.setState(state => ({
-            toggle: !state.toggle
-        }));
-    }
+    setColor = (color) => {
+        if (color === this.state.continColor) {
+            this.setState(state => ({
+                continColor: 'white'
+            }));
+        } else {
+            this.setState(state => ({
+                continColor: color
+            }));
+        }
 
+    }
+ 
     Continue = () => {
-        if (this.state.toggle) 
+        if (this.state.continColor !== 'white')
             this.props.navigation.navigate('BudgetPage');
     }
-
+ 
     render() {
         return (
             <SafeAreaView backgroundColor='#3AA4E0'>
-
+ 
                 {/* Light Blue Section that contains the Dolphin and Header */}
                 <View style={{ backgroundColor: '#3AA4E0', height: hp('100%'), width: wp('100%'), marginTop: -20 }}>
-
+ 
                     {/* The second section of the screen that contains the horizontal scroll bar */}
                     <View style={{ flexDirection: "row", backgroundColor: "#2A7FAE", marginTop: hp('45%'), width: wp('100%'), height: hp('45%'), borderTopLeftRadius: 30 }}>
-
+ 
                         {/* Vertical Line that ends the scrollview */}
                         <View style={{ marginTop: 90, marginLeft: -40, position: 'absolute', height: 8, width: 150, backgroundColor: 'white', borderRadius: 20, transform: [{ rotate: "90deg" }], shadowColor: 'black', shadowRadius: 1, shadowOffset: { height: -2, width: 3 }, shadowOpacity: .5 }} />
-
+ 
                         <ScrollView style={{ position: 'relative', marginLeft: 40, marginTop: 30 }} horizontal={true}>
-
+ 
                             {/* View that contains all the buttons within ScrollView */}
                             <View style={styles.scroll}>
-
+ 
                                 {/* Buttons */}
                                 <TouchableOpacity
-                                    style={{
-                                        backgroundColor: '#9DF5F5',
-                                        height: wp('35%'),
-                                        width: hp('20%'),
-                                        borderRadius: 20,
-                                        alignItems: 'center',
-                                        marginLeft: 20,
-                                        shadowColor: 'white',
-                                        shadowOffset: { width: 4, height: 4 },
-                                        shadowOpacity: .5,
-                                        shadowRadius: 0,
-                                    }}
-                                    onPress={() => navigation.push('BudgetPage')} >
-
+                                    style={[styles.dinDateButton, {opacity: this.state.dinDateOp}]}
+                                    onPress={() => {this.setColor('#9DF5F5'); this.setState(state => ({dinDateOp: 1}))}} >
+ 
                                     <Image
                                         style={{ height: 80, width: 80, marginTop: 10 }}
                                         source={require('./assets/DinnerDate.png')}
                                     />
-
+ 
                                     <View style={styles.bottomTextBorder}>
                                         <Text style={styles.boxText}>Dinner Date</Text>
                                     </View>
+
                                 </TouchableOpacity>
-
+ 
                                 <TouchableOpacity
-                                    style={{
-                                        backgroundColor: '#87A4EF',
-                                        height: wp('35%'),
-                                        width: hp('20%'),
-                                        borderRadius: 20,
-                                        alignItems: 'center',
-                                        marginLeft: 20,
-                                        shadowColor: 'white',
-                                        shadowOffset: { width: 4, height: 4 },
-                                        shadowOpacity: .5,
-                                        shadowRadius: 0,
-                                    }}
-                                    onPress={() => navigation.push('BudgetPage')} >
-
+                                    style={styles.foodAreaButton}
+                                    onPress={() => {this.setColor('#87A4EF');}} >
+ 
                                     <Image
                                         style={{ height: 80, width: 80, marginTop: 10 }}
                                         source={require('./assets/FoodAreas.png')}
                                     />
-
+ 
                                     <View style={styles.bottomTextBorder}>
                                         <Text style={styles.boxText}>Food Areas</Text>
                                     </View>
-
+ 
                                 </TouchableOpacity>
-
+ 
                                 <TouchableOpacity
-                                    style={{
-                                        backgroundColor: '#EAED71',
-                                        height: wp('35%'),
-                                        width: hp('20%'),
-                                        borderRadius: 20,
-                                        alignItems: 'center',
-                                        marginLeft: 20,
-                                        shadowColor: 'white',
-                                        shadowOffset: { width: 4, height: 4 },
-                                        shadowOpacity: .5,
-                                        shadowRadius: 0,
-                                    }}
-                                    onPress={() => navigation.push('BudgetPage')} >
-
+                                    style={styles.VacSpotButton}
+                                    onPress={() => {this.setColor('#EAED71');}} >
+ 
                                     <Image
                                         style={{ height: 80, width: 80, marginTop: 10 }}
                                         source={require('./assets/Vacation.png')}
                                     />
-
+ 
                                     <View style={styles.bottomTextBorder}>
                                         <Text style={styles.boxText}>Vacation Spot</Text>
                                     </View>
                                 </TouchableOpacity>
-
+ 
                                 <TouchableOpacity
-                                    style={{
-                                        backgroundColor: '#74E17F',
-                                        height: wp('35%'),
-                                        width: hp('20%'),
-                                        borderRadius: 20,
-                                        alignItems: 'center',
-                                        marginLeft: 20,
-                                        shadowColor: 'white',
-                                        shadowOffset: { width: 4, height: 4 },
-                                        shadowOpacity: .5,
-                                        shadowRadius: 0,
-                                    }}
-                                    onPress={() => navigation.push('BudgetPage')} >
-
+                                    style={styles.HangSpotButton}
+                                    onPress={() => {this.setColor('#74E17F') }} >
+ 
                                     <Image
                                         style={{ height: 80, width: 80, marginTop: 10 }}
                                         source={require('./assets/HangoutSite.png')}
                                     />
-
+ 
                                     <View style={styles.bottomTextBorder}>
                                         <Text style={styles.boxText}>Hangout Spot</Text>
                                     </View>
                                 </TouchableOpacity>
-
+ 
                             </View>
-
+ 
                         </ScrollView>
-
+ 
                         {/* The final section of the screen that contains the continue button */}
                         <View style={{ alignItems: 'center', position: 'absolute', backgroundColor: "#0E2163", marginTop: hp('28%'), width: wp('100%'), height: hp('30%'), borderTopLeftRadius: 30 }}>
-                            <TouchableOpacity style={styles.continue}
-                            onPress={() => this.Continue()}>
+                            <TouchableOpacity style={[styles.continue, {backgroundColor: this.state.continColor}]}
+                                onPress={() => this.Continue()}>
                                 <Text style={styles.continueText}>Continue</Text>
                             </TouchableOpacity>
                         </View>
-
-
+ 
+ 
                     </View>
-
+ 
                     {/* The Dolphin */}
                     <Image style={styles.dolphin}
                         source={require('./assets/dolphin.png')} />
-
+ 
                     {/* The Header */}
                     <Text style={styles.header}>
                         Categories
                 </Text>
-
+ 
                 </View>
-
+ 
             </SafeAreaView>
         );
     }
 }
-
-
+ 
+ 
 const styles = StyleSheet.create({
-
-  dolphin: {
-    position: 'absolute',
-    transform: [{translateX: 20}, {translateY: -110}, {rotate: "-30deg"}, {scale: .7}]
-  },
-
-  header: {
-    position: 'absolute',
-    color: 'white',
-    marginTop: hp ('37%'),
-    marginLeft: wp ('4%'),
-    fontSize: 35,
-  },
-
-  buttons: {
-    backgroundColor: '#9DF5F5',
-    height: wp ('35%'),
-    width: hp ('20%'),
-    borderRadius: 20,
-    alignItems: 'center',
-    marginLeft: 20,
-    shadowColor: 'white',
-    shadowOffset: {width: 4, height: 4},
-    shadowOpacity: .5,
-    shadowRadius: 0,
-  },
-  
-  bottomTextBorder: {
-      backgroundColor: 'white',
-      marginTop: 10,
-      width: hp ('20%'),
-      height: wp ('8.4%'),
-      alignItems: "center",
-      borderBottomLeftRadius: 20, 
-      borderBottomRightRadius: 20,
-  },
-
-  boxText: {
-      marginTop: 3,
-      fontSize: 18
-  },
-
-  scroll: {
-    flexDirection: 'row',
-    height: 200,
-    marginRight: 20,
-  },
-
-  continue: {
-      marginTop: 17,
-      backgroundColor: 'white',
-      height: hp ('5%'),
-      width: wp ('90%'),
-      alignItems: 'center',
-      borderRadius: 40,
-      shadowColor: 'white',
-      shadowOffset: {width: 3, height: 3},
-      shadowOpacity: .5,
-      shadowRadius: 0,
-  },
-
-  continueText: {
-    fontSize: 25,
-  }
+ 
+    dolphin: {
+        position: 'absolute',
+        transform: [{ translateX: 20 }, { translateY: -110 }, { rotate: "-30deg" }, { scale: .7 }]
+    },
+ 
+    header: {
+        position: 'absolute',
+        color: 'white',
+        marginTop: hp('37%'),
+        marginLeft: wp('4%'),
+        fontSize: 35,
+    },
+ 
+    buttons: {
+        backgroundColor: '#9DF5F5',
+        height: wp('35%'),
+        width: hp('20%'),
+        borderRadius: 20,
+        alignItems: 'center',
+        marginLeft: 20,
+        shadowColor: 'white',
+        shadowOffset: { width: 4, height: 4 },
+        shadowOpacity: .5,
+        shadowRadius: 0,
+    },
+ 
+    bottomTextBorder: {
+        backgroundColor: 'white',
+        marginTop: 10,
+        width: hp('20%'),
+        height: wp('8.4%'),
+        alignItems: "center",
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20,
+    },
+ 
+    boxText: {
+        marginTop: 3,
+        fontSize: 18
+    },
+ 
+    scroll: {
+        flexDirection: 'row',
+        height: 200,
+        marginRight: 20,
+    },
+ 
+    continue: {
+        marginTop: 17,
+        height: hp('5%'),
+        width: wp('90%'),
+        alignItems: 'center',
+        borderRadius: 40,
+        shadowColor: 'white',
+        shadowOffset: { width: 3, height: 3 },
+        shadowOpacity: .5,
+        shadowRadius: 0,
+    },
+ 
+    continueText: {
+        fontSize: 25,
+    },
+ 
+    dinDateButton: {
+        backgroundColor: '#9DF5F5',
+        height: wp('35%'),
+        width: hp('20%'),
+        borderRadius: 20,
+        alignItems: 'center',
+        marginLeft: 20,
+        shadowColor: 'white',
+        shadowOffset: { width: 4, height: 4 },
+        shadowOpacity: .5,
+        shadowRadius: 0,
+    },
+ 
+    foodAreaButton: {
+        backgroundColor: '#87A4EF',
+        height: wp('35%'),
+        width: hp('20%'),
+        borderRadius: 20,
+        alignItems: 'center',
+        marginLeft: 20,
+        shadowColor: 'white',
+        shadowOffset: { width: 4, height: 4 },
+        shadowOpacity: .5,
+        shadowRadius: 0,
+    },
+ 
+    VacSpotButton: {
+        backgroundColor: '#EAED71',
+        height: wp('35%'),
+        width: hp('20%'),
+        borderRadius: 20,
+        alignItems: 'center',
+        marginLeft: 20,
+        shadowColor: 'white',
+        shadowOffset: { width: 4, height: 4 },
+        shadowOpacity: .5,
+        shadowRadius: 0,
+    },
+ 
+    HangSpotButton: {
+        backgroundColor: '#74E17F',
+        height: wp('35%'),
+        width: hp('20%'),
+        borderRadius: 20,
+        alignItems: 'center',
+        marginLeft: 20,
+        shadowColor: 'white',
+        shadowOffset: { width: 4, height: 4 },
+        shadowOpacity: .5,
+        shadowRadius: 0,
+    },
+ 
 });
