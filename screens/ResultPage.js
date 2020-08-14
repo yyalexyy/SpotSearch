c74a860e7e01ec068c1f0801bccb032c08b0e383
rrosyidi <ravioli4stuff@gmail.com>
2020-08-14 17:56:45 -0400

    Max Time Page added coverter from time to meters for radius

--- screens/ResultPage.js
+++ screens/ResultPage.js
@@ -12,28 +12,45 @@ import {
   DrawerItem,
 } from '@react-navigation/drawer';
 
-
 /**
 * Result Screen
 * @param {*} param0 
 */
-export class ResultPage extends React.Component{
-  constructor(props){
+export class ResultPage extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
       data: [],
       option: props.route.params.option,
       cost: props.route.params.cost,
-      selectedHours: props.route.params.selectedHours,
-      selectedMinutes: props.route.params.selectedMinutes,
+      radius: props.route.params.radius,
+      ready: false,
+      loca: { lat: null, log: null },
+      error: null
     }
   }
-  
 
+  componentDidMount() {
+    let geoOptions = {
+      enableHighAccuracy: true,
+      timeOut: 20000,   //20 sec
+      maximumAge: 60 * 60 * 24
+    }
+    this.setState({ ready: false, error: null });
+    navigator.geolocation.getCurrentPosition(this.locaSuccess, this.locaFail, geoOptions);
+  }
 
+  locaSuccess = (position) => {
+    console.log(position.coords.latitude, position.coords.longitude);
 
-  componentDidMount(){
-    this.fetchData();
+    this.setState({
+      ready: true,
+      loca: { lat: position.coords.latitude, log: position.coords.longitude }
+    })
+  }
+
+  locaFail = (error) => {
+    this.setState({ error: error.message })
   }
 
   fetchData = async () => {
@@ -53,52 +70,61 @@ export class ResultPage extends React.Component{
     //   console.error(err);
     // }
 
-
-    const response = await fetch("https://api.scaleserp.com/search?api_key=9419519988374534B1FE7DFCD3D80C2A&q=Vacation+Spot");
+    const response = await fetch("https://api.scaleserp.com/search?api_key=demo&q=bitcoin");
     const json = await response.json();
-    this.setState( {data: json.organic_results} );
+    this.setState({ data: json.organic_results });
   }
 
-
-  render(){
-      return(
-        <SafeAreaView backgroundColor = '#116466'>
-          {/* <FlatList
+  render() {
+    return (
+      <SafeAreaView backgroundColor='#116466'>
+        {/* <FlatList
             data={this.state.data}
             keyExtractor={(x,i) => i}
             renderItem={ ({ item }) =>
               <Text >{item.title}</Text>}
           /> */}
 
-          <Text style={{textAlign: 'center'}}> {this.state.cost}, {this.state.option},{this.state.selectedHours},{this.state.selectedMinutes} </Text>
+        <Text style={{ textAlign: 'center' }}> {this.state.cost}, {this.state.option},{this.state.radius} </Text>
 
+        <Text style={{ textAlign: 'center' }}>
+          Chick-Fil-A
+          </Text>
 
-          <Text style={{textAlign: 'center'}}>
-                Chick-fil-A
-            </Text>
+          { !this.state.ready && (
+            <Text style={styles.big}>Using GeoLocation in REACT</Text>
+          )}
 
-        </SafeAreaView>
-      );
+          { this.state.error && (
+            <Text style={styles.big}>{this.state.error}</Text>
+          )} 
+
+          { this.state.ready && (
+            <Text style={styles.big}>{`Latitude: ${this.state.loca.lat} Longitude: ${this.state.loca.log}`}</Text>
+          )}  
+
+      </SafeAreaView>
+    );
 
   }
 }
 
 
-const styles = StyleSheet.create({  
-    container:{
-      flex: 1,
-      backgroundColor: '#F5FCFF',
-      alignItems: 'center',
-      justifyContent: 'center',
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+    alignItems: 'center',
+    justifyContent: 'center',
 
-    },
-    item:{
-      flex: 1,
-      alignSelf: 'stretch',
-      margin: 10,
-      alignItems: 'center',
-      justifyContent: 'center',
-      borderBottomWidth: 1,
-    }
+  },
+  item: {
+    flex: 1,
+    alignSelf: 'stretch',
+    margin: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomWidth: 1,
+  }
 
-  });
+});
