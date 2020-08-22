38c20dd59f3404b4bfdf72292633f02a544ff0de
yyalexyy <44172655+ajuan3@users.noreply.github.com>
2020-08-21 21:20:38 -0400

    Adding swiper and hdiing top nav in resultPage, and updated screens

--- screens/HomePageRedesign.js
+++ screens/HomePageRedesign.js
@@ -41,51 +41,59 @@ export class HomePageRedesign extends React.Component {
     }
 
     Continue = () => {
-        if(this.state.option === "Dinner Date" || this.state.option === "Food Area"){
-            this.setState((state) => {return { option: "restaurant" }});
+        if (this.state.option === "Dinner Date" || this.state.option === "Food Area") {
+            this.setState((state) => { return { option: "restaurant" } });
+        }
+        else if(this.state.option === "Vacation Spot" || this.state.option === "Hangout Spot"){
+            this.setState((state) => {return { option: "amusement_park" }});
         }
 
         if (this.state.continColor !== 'white')
-                this.props.navigation.navigate('BudgetPage', {option: this.state.option});
+            this.props.navigation.navigate('BudgetPage', { option: this.state.option });
     }
 
     toggle = (color) => {
         if (color === 'DD') {
-            this.state.dinDateOp === .5 ? this.setState((state) => {return { dinDateOp: 1, foodAreaOp: .5, vacSpotOp: .5, hangOutOp: .5 }}) : this.setState((state) => {return { dinDateOp: .5 }});
+            this.state.dinDateOp === .5 ? this.setState((state) => { return { dinDateOp: 1, foodAreaOp: .5, vacSpotOp: .5, hangOutOp: .5 } }) : this.setState((state) => { return { dinDateOp: .5 } });
         } else if (color === 'FA') {
-            this.state.foodAreaOp === .5 ? this.setState((state) => {return { dinDateOp: .5, foodAreaOp: 1, vacSpotOp: .5, hangOutOp: .5 }}) : this.setState((state) => {return {foodAreaOp: .5}});
+            this.state.foodAreaOp === .5 ? this.setState((state) => { return { dinDateOp: .5, foodAreaOp: 1, vacSpotOp: .5, hangOutOp: .5 } }) : this.setState((state) => { return { foodAreaOp: .5 } });
         } else if (color === 'VS') {
-            this.state.vacSpotOp === .5 ? this.setState((state) => {return {dinDateOp: .5, foodAreaOp: .5, vacSpotOp: 1, hangOutOp: .5 }}) : this.setState((state) => {return {vacSpotOp: .5}});
+            this.state.vacSpotOp === .5 ? this.setState((state) => { return { dinDateOp: .5, foodAreaOp: .5, vacSpotOp: 1, hangOutOp: .5 } }) : this.setState((state) => { return { vacSpotOp: .5 } });
         } else if (color === 'HS') {
-            this.state.hangOutOp === .5 ? this.setState((state) => {return { dinDateOp: .5, foodAreaOp: .5, vacSpotOp: .5, hangOutOp: 1 }}) : this.setState((state) => {return { hangOutOp: .5 }});
+            this.state.hangOutOp === .5 ? this.setState((state) => { return { dinDateOp: .5, foodAreaOp: .5, vacSpotOp: .5, hangOutOp: 1 } }) : this.setState((state) => { return { hangOutOp: .5 } });
         }
     }
 
     render() {
         return (
             <SafeAreaView backgroundColor='#3AA4E0'>
- 
+                {/* Faint line below "Catergories" */}
+                <View style={{ position: 'relative', marginTop: -20, marginBottom: 15, height: 2, backgroundColor: 'white', marginLeft: 0, marginRight: 0, zIndex: 999, opacity: .7 }} />
+
+                {/* Faint line below "Catergories" */}
+                <View style={{ position: 'absolute', marginTop: hp('44.5%'), height: 2, marginLeft: wp('5%'), width: 340, marginRight: wp('5%'), backgroundColor: 'white', zIndex: 999, opacity: .5 }} />
+
                 {/* Light Blue Section that contains the Dolphin and Header */}
-                <View style={{ backgroundColor: '#3AA4E0', height: hp('100%'), width: wp('100%'), marginTop: -20 }}>
- 
+                <View style={{ backgroundColor: '#91C6E4', height: hp('100%'), width: wp('100%'), marginTop: -20 }}>
+
                     {/* The second section of the screen that contains the horizontal scroll bar */}
-                    <View style={{ flexDirection: "row", backgroundColor: "#2A7FAE", marginTop: hp('45%'), width: wp('100%'), height: hp('45%'), borderTopLeftRadius: 30 }}>
- 
+                    <View style={{ flexDirection: "row", backgroundColor: "#91C6E4", marginTop: hp('45%'), width: wp('100%'), height: hp('45%'), borderTopLeftRadius: 30 }}>
+
                         {/* Vertical Line that ends the scrollview */}
                         <View style={{ marginTop: 90, marginLeft: -40, position: 'absolute', height: 8, width: 150, backgroundColor: 'white', borderRadius: 20, transform: [{ rotate: "90deg" }], shadowColor: 'black', shadowRadius: 1, shadowOffset: { height: -2, width: 3 }, shadowOpacity: .5, zIndex: 999 }} />
- 
+
                         <ScrollView style={{ position: 'relative', marginLeft: 40, marginTop: 20, height: 400 }} horizontal={true}>
- 
+
                             {/* View that contains all the buttons within ScrollView */}
                             <View style={styles.scroll}>
- 
+
                                 {/* Buttons */}
                                 <TouchableOpacity
-                                    onPress={() => {this.setColor('#9DF5F5'); this.toggle('DD'); this.setState(state => ({option: 'Dinner Date'})); }} 
-                                    style={this.state.dinDateOp === 1 ? styles.dinDateButtonPressed : styles.preButton} 
+                                    onPress={() => { this.setColor('#9DF5F5'); this.toggle('DD'); this.setState(state => ({ option: 'Dinner Date' })); }}
+                                    style={this.state.dinDateOp === 1 ? styles.dinDateButtonPressed : styles.preButton}
                                     activeOpacity={.8} >
-                                    <View style={{alignItems: 'center'}}>
- 
+                                    <View style={{ alignItems: 'center' }}>
+
                                         <Image
                                             style={{ height: 80, width: 80, marginTop: 10 }}
                                             source={require('./assets/DinnerDate.png')}
@@ -99,9 +107,9 @@ export class HomePageRedesign extends React.Component {
 
                                 <TouchableOpacity
                                     style={this.state.foodAreaOp === 1 ? styles.foodAreaButtonPressed : styles.preButton}
-                                    onPress={() => { this.setColor('#87A4EF'); this.toggle('FA'); this.setState(state => ({option: 'Food Area'})); }} 
+                                    onPress={() => { this.setColor('#87A4EF'); this.toggle('FA'); this.setState(state => ({ option: 'Food Area' })); }}
                                     activeOpacity={.8} >
-                                    <View style={{alignItems: 'center' }}>
+                                    <View style={{ alignItems: 'center' }}>
                                         <Image
                                             style={{ height: 80, width: 80, marginTop: 10 }}
                                             source={require('./assets/FoodAreas.png')}
@@ -115,7 +123,7 @@ export class HomePageRedesign extends React.Component {
 
                                 <TouchableOpacity
                                     style={this.state.vacSpotOp === 1 ? styles.VacSpotButtonPressed : styles.preButton}
-                                    onPress={() => { this.setColor('#EAED71'); this.toggle('VS'); this.setState(state => ({option: 'Vacation Spot'})); }} 
+                                    onPress={() => { this.setColor('#EAED71'); this.toggle('VS'); this.setState(state => ({ option: 'Vacation Spot' })); }}
                                     activeOpacity={.8} >
                                     <View style={{ alignItems: 'center' }}>
                                         <Image
@@ -131,9 +139,9 @@ export class HomePageRedesign extends React.Component {
 
                                 <TouchableOpacity
                                     style={this.state.hangOutOp === 1 ? styles.HangSpotButtonPressed : styles.preButton}
-                                    onPress={() => { this.setColor('#F08C44'); this.toggle('HS'); this.setState(state => ({option: 'Hangout Spot'})); }} 
+                                    onPress={() => { this.setColor('#F08C44'); this.toggle('HS'); this.setState(state => ({ option: 'Hangout Spot' })); }}
                                     activeOpacity={.8} >
-                                    <View style={{alignItems: 'center' }}>
+                                    <View style={{ alignItems: 'center' }}>
                                         <Image
                                             style={{ height: 80, width: 80, marginTop: 10 }}
                                             source={require('./assets/fireplace.png')}
@@ -144,54 +152,53 @@ export class HomePageRedesign extends React.Component {
                                         </View>
                                     </View>
                                 </TouchableOpacity>
- 
+
                             </View>
- 
+
                         </ScrollView>
- 
+
                         {/* The final section of the screen that contains the continue button */}
-                        <View style={{ alignItems: 'center', position: 'absolute', backgroundColor: "#0E2163", marginTop: hp('28%'), width: wp('100%'), height: hp('30%'), borderTopLeftRadius: 30 }}>
-                            <TouchableOpacity style={[styles.continue, {backgroundColor: this.state.continColor}]}
+                        <View style={{ alignItems: 'center', position: 'absolute', backgroundColor: "#609FC2", marginTop: hp('28%'), width: wp('100%'), height: hp('30%'), borderTopLeftRadius: 30 }}>
+                            <TouchableOpacity style={[styles.continue, { backgroundColor: this.state.continColor }]}
                                 onPress={() => this.Continue()}>
                                 <Text style={styles.continueText}>Continue</Text>
                             </TouchableOpacity>
                         </View>
- 
- 
+
                     </View>
- 
+
                     {/* The Dolphin */}
                     <Image style={styles.dolphin}
                         source={require('./assets/dolphin.png')} />
- 
+
                     {/* The Header */}
                     <Text style={styles.header}>
                         Categories
-                </Text>
- 
+                    </Text>
+
                 </View>
- 
+
             </SafeAreaView>
         );
     }
 }
- 
- 
+
+
 const styles = StyleSheet.create({
- 
+
     dolphin: {
         position: 'absolute',
         transform: [{ translateX: 20 }, { translateY: -110 }, { rotate: "-30deg" }, { scale: .7 }]
     },
- 
+
     header: {
         position: 'absolute',
         color: 'white',
-        marginTop: hp('37%'),
+        marginTop: hp('36%'),
         marginLeft: wp('4%'),
-        fontSize: 35,
+        fontSize: 40,
     },
- 
+
     bottomTextBorder: {
         backgroundColor: 'white',
         marginTop: 10,
@@ -201,18 +208,18 @@ const styles = StyleSheet.create({
         borderBottomLeftRadius: 20,
         borderBottomRightRadius: 20,
     },
- 
+
     boxText: {
         marginTop: 3,
         fontSize: 18
     },
- 
+
     scroll: {
         flexDirection: 'row',
         height: 200,
         marginRight: 20,
     },
- 
+
     continue: {
         marginTop: 17,
         height: hp('5%'),
@@ -224,7 +231,7 @@ const styles = StyleSheet.create({
         shadowOpacity: .5,
         shadowRadius: 0,
     },
- 
+
     continueText: {
         fontSize: 25,
     },
@@ -253,11 +260,11 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         marginLeft: 20,
         shadowColor: 'white',
-        shadowOffset: {height: .5},
+        shadowOffset: { height: .5 },
         shadowOpacity: 1,
         shadowRadius: 4,
     },
- 
+
     foodAreaButton: {
         backgroundColor: '#ffffff',
         height: wp('35%'),
@@ -282,11 +289,11 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         marginLeft: 20,
         shadowColor: 'white',
-        shadowOffset: {height: .5},
+        shadowOffset: { height: .5 },
         shadowOpacity: 1,
         shadowRadius: 5,
     },
- 
+
     VacSpotButton: {
         backgroundColor: '#ffffff',
         height: wp('35%'),
@@ -311,11 +318,11 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         marginLeft: 20,
         shadowColor: 'white',
-        shadowOffset: {height: .5},
+        shadowOffset: { height: .5 },
         shadowOpacity: 1,
         shadowRadius: 5,
     },
- 
+
     HangSpotButton: {
         backgroundColor: '#ffffff',
         height: wp('35%'),
@@ -340,9 +347,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         marginLeft: 20,
         shadowColor: 'white',
-        shadowOffset: {height: .5},
+        shadowOffset: { height: .5 },
         shadowOpacity: 1,
         shadowRadius: 5,
     },
- 
+
 });
--- screens/MainTabScreen.js
+++ screens/MainTabScreen.js
@@ -74,7 +74,7 @@ export default MainTabScreen;
 const HomeStackScreen = ({ navigation }) => (
     <HomeStack.Navigator screenOptions={{
         headerStyle:{
-          backgroundColor: '#3AA4E0',
+          backgroundColor: '#91C6E4',
           shadowColor: 'transparent',
         },
         headerTintColor: 'black',     //back button color
@@ -90,7 +90,7 @@ const HomeStackScreen = ({ navigation }) => (
               name= "menu"
               size={25}
               color="#000000"           //menu tab color
-              backgroundColor= "#3AA4E0"
+              backgroundColor= "#91C6E4"
               onPress={() => navigation.toggleDrawer()}/>
           )
           }} />
@@ -102,7 +102,7 @@ const HomeStackScreen = ({ navigation }) => (
               name= "menu"
               size={25}
               color="#000000"           //menu tab color
-              backgroundColor= "#3AA4E0"
+              backgroundColor= "#91C6E4"
               onPress={() => navigation.toggleDrawer()}/>
           )
         }} />
@@ -112,7 +112,7 @@ const HomeStackScreen = ({ navigation }) => (
               name= "menu"
               size={25}
               color="#000000"           //menu tab color
-              backgroundColor= "#3AA4E0"
+              backgroundColor= "#91C6E4"
               onPress={() => navigation.toggleDrawer()}/>
           )
         }}  />
@@ -138,15 +138,16 @@ const HomeStackScreen = ({ navigation }) => (
           )
         }} />
         <HomeStack.Screen name="ResultPage" component={ResultPage} options={{ 
-          title:"Spot",
-          headerRight: () => (
-            <Icon.Button 
-              name= "menu"
-              size={25}
-              color="#000000"           //menu tab color
-              backgroundColor= "#3AA4E0"
-              onPress={() => navigation.toggleDrawer()}/>
-          )
+          // title:"Spot",
+          headerShown: false,
+          // headerRight: () => (
+          //   <Icon.Button 
+          //     name= "menu"
+          //     size={25}
+          //     color="#000000"           //menu tab color
+          //     backgroundColor= "#3AA4E0"
+          //     onPress={() => navigation.toggleDrawer()}/>
+          // )
         }} />
     </HomeStack.Navigator>
   )
@@ -154,7 +155,7 @@ const HomeStackScreen = ({ navigation }) => (
   const RecentStackScreen = ({ navigation }) => (
     <RecentStack.Navigator screenOptions={{
         headerStyle:{
-          backgroundColor: '#C5CBE3',
+          backgroundColor: '#91C6E4',
         },
         //headerTintColor: '#000000',
         headerTitleStyle: {
@@ -168,7 +169,7 @@ const HomeStackScreen = ({ navigation }) => (
               name= "menu"
               size={25}
               color="#ffffff"
-              backgroundColor= "#C5CBE3"
+              backgroundColor= "#91C6E4"
               onPress={() => navigation.toggleDrawer()}/>
           )
           }} />
@@ -179,7 +180,7 @@ const HomeStackScreen = ({ navigation }) => (
   const FavoritesStackScreen = ({ navigation }) => (
     <FavoritesStack.Navigator screenOptions={{
         headerStyle:{
-          backgroundColor: '#C5CBE3',
+          backgroundColor: '#91C6E4',
         },
         //headerTintColor: '#000000',
         headerTitleStyle: {
@@ -193,7 +194,7 @@ const HomeStackScreen = ({ navigation }) => (
               name= "menu"
               size={25}
               color="#ffffff"
-              backgroundColor= "#C5CBE3"
+              backgroundColor= "#91C6E4"
               onPress={() => navigation.toggleDrawer()}/>
           )
           }} />
