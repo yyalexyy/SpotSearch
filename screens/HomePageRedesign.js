23cd8c4bf91e81952aba69da249bc87730f5d86a
rrosyidi <ravioli4stuff@gmail.com>
2021-01-22 19:05:31 -0500

    HomePage redesigned better code structured

--- screens/HomePageRedesign.js
+++ screens/HomePageRedesign.js
@@ -10,80 +10,31 @@ export class HomePageRedesign extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            finDineOp: .5,
-            casBitesOp: .5,
-            recOp: .5,
-            landmarksOp: .5,
+            button: "",
             continColor: 'white',
-            option: '...',
+            option: "",
         };
-    }
-
-    setColor = (color) => {
-        if (color === this.state.continColor) {
-            this.setState(state => ({
-                continColor: 'white'
-            }));
-        } else {
-            this.setState(state => ({
-                continColor: color
-            }));
-        }
-
-    }
-
-    /**
-     * Route to Budget Screen
-     */
-    toFineDining = () => {
-        this.setState((state) => { return { option: "restaurant"} }, () => {this.props.navigation.navigate('BudgetPage', { option: this.state.option })});
-       
-    }
-    /**
-     * Route to Casual Bites Category Screen
-     */
-    toCasualBites = () => {
-        this.setState((state) => { return { option: "bakery" } }, () => {this.props.navigation.navigate('CasualBitesTypes', { option: this.state.option })});
-    }
-    /**
-     * Route to Recreation Category Screen
-     */
-    toRecreation = () => {
-        this.setState((state) => { return { option: "amusement_park" } }, () => {this.props.navigation.navigate('RecreationTypes', { option: this.state.option })});
-    }
-    /**
-     * Route to LandMarks Screen
-     */
-    toLandMarks = () => {
-        this.setState((state) => { return { option: "tourist_attraction" } }, () => {this.props.navigation.navigate('MaxTimePage', { option: this.state.option })});
     }
 
     Continue = () => {
         if (this.state.continColor !== 'white') {
-            if (this.state.option === 'Fine Dining') {
-                this.toFineDining()
+            this.setState({continColor: 'white'})
+            if (this.state.button === 'Restaurant') {
+                this.setState({button: ''})
+                this.setState({ option: "restaurant"} , () => {this.props.navigation.navigate('BudgetPage', { option: "restaurant" })});
             }
-            else if (this.state.option === 'Casual Bites') {
-                this.toCasualBites()
+            else if (this.state.button === 'Casual Bites') {
+                this.setState({button: ''})
+                this.setState({ option: "Casual Bites"} , () => {this.props.navigation.navigate('CasualBitesTypes', { option: "Casual Bites" })});
             }
-            else if (this.state.option === 'Recreation') {
-                this.toRecreation()
+            else if (this.state.button === 'Recreation') {
+                this.setState({button: ''})
+                this.setState({ option: "Recreation"} , () => {this.props.navigation.navigate('RecreationTypes', { option: "Recreation" })});
             }
-            else if (this.state.option === 'Landmarks') {
-                this.toLandMarks()
+            else if (this.state.button === 'Landmarks') {
+                this.setState({button: ''})
+                this.setState({ option: "restaurant"} , () => {this.props.navigation.navigate('MaxTimePage', { option: "restaurant" })});
             }
-        }
-    }
-
-    toggle = (color) => {
-        if (color === 'FD') {
-            this.state.finDineOp === .5 ? this.setState((state) => { return { finDineOp: 1, casBitesOp: .5, recOp: .5, landmarksOp: .5 } }) : this.setState((state) => { return { finDineOp: .5 } });
-        } else if (color === 'CB') {
-            this.state.casBitesOp === .5 ? this.setState((state) => { return { finDineOp: .5, casBitesOp: 1, recOp: .5, landmarksOp: .5 } }) : this.setState((state) => { return { casBitesOp: .5 } });
-        } else if (color === 'Rec') {
-            this.state.recOp === .5 ? this.setState((state) => { return { finDineOp: .5, casBitesOp: .5, recOp: 1, landmarksOp: .5 } }) : this.setState((state) => { return { recOp: .5 } });
-        } else if (color === 'LM') {
-            this.state.landmarksOp === .5 ? this.setState((state) => { return { finDineOp: .5, casBitesOp: .5, recOp: .5, landmarksOp: 1 } }) : this.setState((state) => { return { landmarksOp: .5 } });
         }
     }
 
@@ -112,8 +63,8 @@ export class HomePageRedesign extends React.Component {
 
                                     {/* Buttons */}
                                     <TouchableOpacity
-                                        onPress={() => { this.setColor('#9DF5F5'); this.toggle('FD'); this.setState(state => ({ option: 'Fine Dining' })); }}
-                                        style={this.state.finDineOp === 1 ? styles.finDineButtonPressed : styles.preButton}
+                                        onPress={() => { this.state.button === "Restaurant" ? this.setState({button:"", continColor: "white"}) : this.setState({button: "Restaurant", continColor: "#9DF5F5"}) }}
+                                        style={ this.state.button === "Restaurant" ? [styles.preButton, styles.buttonPressed, {backgroundColor: '#9DF5F5', shadowColor: '#9DF5F5'} ] : styles.preButton}
                                         activeOpacity={.8} >
                                         <View style={{ alignItems: 'center' }}>
 
@@ -129,8 +80,8 @@ export class HomePageRedesign extends React.Component {
                                     </TouchableOpacity>
 
                                     <TouchableOpacity
-                                        style={this.state.casBitesOp === 1 ? styles.casBitesButtonPressed : styles.preButton}
-                                        onPress={() => { this.setColor('#87A4EF'); this.toggle('CB'); this.setState(state => ({ option: 'Casual Bites' })); }}
+                                        style={this.state.button === "Casual Bites" ? [styles.preButton, styles.buttonPressed, {backgroundColor: '#87A4EF', shadowColor: '#87A4EF'}] : styles.preButton}
+                                        onPress={() => { this.state.button === "Casual Bites" ? this.setState({button:"", continColor: "white"}) : this.setState({button: "Casual Bites", continColor: "#87A4EF"}) }}
                                         activeOpacity={.8} >
                                         <View style={{ alignItems: 'center' }}>
                                             <Image
@@ -145,8 +96,8 @@ export class HomePageRedesign extends React.Component {
                                     </TouchableOpacity>
 
                                     <TouchableOpacity
-                                        style={this.state.recOp === 1 ? styles.recButtonPressed : styles.preButton}
-                                        onPress={() => { this.setColor('#EAED71'); this.toggle('Rec'); this.setState(state => ({ option: 'Recreation' })); }}
+                                        style={this.state.button === "Recreation" ? [styles.preButton, styles.buttonPressed, {backgroundColor: '#EAED71', shadowColor: '#EAED71'}] : styles.preButton}
+                                        onPress={() => { this.state.button === "Recreation" ? this.setState({button:"", continColor: "white"}) : this.setState({button: "Recreation", continColor: "#EAED71"}) }}
                                         activeOpacity={.8} >
                                         <View style={{ alignItems: 'center' }}>
                                             <Image
@@ -161,8 +112,8 @@ export class HomePageRedesign extends React.Component {
                                     </TouchableOpacity>
 
                                     <TouchableOpacity
-                                        style={this.state.landmarksOp === 1 ? styles.landmarksButtonPressed : styles.preButton}
-                                        onPress={() => { this.setColor('#F08C44'); this.toggle('LM'); this.setState(state => ({ option: 'Landmarks' })); }}
+                                        style={ this.state.button === "Landmarks" ? [styles.preButton, styles.buttonPressed, { backgroundColor: '#F08C44', shadowColor: '#F08C44'}] : styles.preButton}
+                                        onPress={() => { this.state.button === "Landmarks" ? this.setState({button:"", continColor: "white"}) : this.setState({button: "Landmarks", continColor: "#F08C44"}) }}
                                         activeOpacity={.8} >
                                         <View style={{ alignItems: 'center' }}>
                                             <Image
@@ -205,6 +156,14 @@ export class HomePageRedesign extends React.Component {
 
 
 const styles = StyleSheet.create({
+
+    buttonPressed: {
+        shadowOffset: { width: .5, height: .5 },
+        shadowOpacity: 1,
+        shadowRadius: 7,
+        top: 9, 
+        left: 4,
+    },
 
     dolphin: {
         position: 'absolute',
@@ -269,108 +228,6 @@ const styles = StyleSheet.create({
         shadowOffset: { width: 4, height: 4 },
         shadowOpacity: .5,
         shadowRadius: 0,
-    },
-
-    finDineButtonPressed: {
-        backgroundColor: '#9DF5F5',
-        top: 9,
-        left: 4,
-        height: wp('35%'),
-        width: hp('20%'),
-        borderRadius: 20,
-        alignItems: 'center',
-        marginLeft: 20,
-        shadowColor: '#9DF5F5',
-        shadowOffset: { width: .5, height: .5 },
-        shadowOpacity: 1,
-        shadowRadius: 7,
-    },
-
-    foodAreaButton: {
-        backgroundColor: '#ffffff',
-        height: wp('35%'),
-        width: hp('20%'),
-        borderRadius: 20,
-        alignItems: 'center',
-        marginLeft: 20,
-        top: 5,
-        shadowColor: 'white',
-        shadowOffset: { width: 4, height: 4 },
-        shadowOpacity: .5,
-        shadowRadius: 0,
-    },
-
-    casBitesButtonPressed: {
-        backgroundColor: '#87A4EF',
-        top: 9,
-        left: 4,
-        height: wp('35%'),
-        width: hp('20%'),
-        borderRadius: 20,
-        alignItems: 'center',
-        marginLeft: 20,
-        shadowColor: '#87A4EF',
-        shadowOffset: { width: .5, height: .5 },
-        shadowOpacity: 1,
-        shadowRadius: 7,
-    },
-
-    VacSpotButton: {
-        backgroundColor: '#ffffff',
-        height: wp('35%'),
-        width: hp('20%'),
-        borderRadius: 20,
-        alignItems: 'center',
-        marginLeft: 20,
-        top: 5,
-        shadowColor: 'white',
-        shadowOffset: { width: 4, height: 4 },
-        shadowOpacity: .5,
-        shadowRadius: 0,
-    },
-
-    recButtonPressed: {
-        backgroundColor: '#EAED71',
-        top: 9,
-        left: 4,
-        height: wp('35%'),
-        width: hp('20%'),
-        borderRadius: 20,
-        alignItems: 'center',
-        marginLeft: 20,
-        shadowColor: '#EAED71',
-        shadowOffset: { width: .5, height: .5 },
-        shadowOpacity: 1,
-        shadowRadius: 7,
-    },
-
-    HangSpotButton: {
-        backgroundColor: '#ffffff',
-        height: wp('35%'),
-        width: hp('20%'),
-        borderRadius: 20,
-        alignItems: 'center',
-        marginLeft: 20,
-        top: 5,
-        shadowColor: 'white',
-        shadowOffset: { width: 4, height: 4 },
-        shadowOpacity: .5,
-        shadowRadius: 0,
-    },
-
-    landmarksButtonPressed: {
-        backgroundColor: '#F08C44',
-        top: 9,
-        left: 4,
-        height: wp('35%'),
-        width: hp('20%'),
-        borderRadius: 20,
-        alignItems: 'center',
-        marginLeft: 20,
-        shadowColor: '#F08C44',
-        shadowOffset: { width: .5, height: .5 },
-        shadowOpacity: 1,
-        shadowRadius: 7,
     },
 
 });
